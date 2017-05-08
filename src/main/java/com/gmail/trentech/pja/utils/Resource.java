78f7bd779249fea6c832615ffe1d39cba4d1b64a
BrainStone <yannick@tedworld.de>
2017-05-08 09:30:59 +0200

    Updated to API version 7.0.0

--- src/main/java/com/gmail/trentech/pja/utils/Resource.java
+++ src/main/java/com/gmail/trentech/pja/utils/Resource.java
@@ -4,7 +4,7 @@ public class Resource {
 
 	public final static String NAME = "Project Automation";
 	public final static String VERSION = "@project.version@";
-	public final static String ID = "com.gmail.trentech.pja";
+	public final static String ID = "pja";
 	public final static String AUTHOR = "TrenTech";
 	public final static String URL = "http://github.com/TrenTech/ProjectAutomation";
 	public final static String DESCRIPTION = "Create commands using signs, buttons, schedulers etc...";
